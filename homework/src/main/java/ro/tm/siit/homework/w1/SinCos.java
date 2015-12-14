064c68bc58009d26efb57721e0c377c76c042ebd
Madalin Cosma <madalin.cosma@dialogdata.de>
2015-12-14 14:53:26 +0200

    homework T1 - reference impl

---
+++ homework/src/main/java/ro/tm/siit/homework/w1/SinCos.java
@@ -0,0 +1,10 @@
+package ro.tm.siit.homework.w1;
+
+public class SinCos {
+	
+	public static void main(String... args) {
+		double x = Double.parseDouble(args[0]);
+		double rezultat = Math.pow(Math.sin(x), 2) + Math.pow(Math.cos(x), 2);
+		System.out.println(rezultat); // 1 or  0.9999..
+	}
+}
