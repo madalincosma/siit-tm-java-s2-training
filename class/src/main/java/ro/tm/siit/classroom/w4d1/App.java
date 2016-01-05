9a350332e6053cbf0ea7c5aeac78f165ea3348b6
mco <madalin.cosma@dialogdata.de>
2016-01-05 12:19:51 +0200

    added examples from training @ day 7

---
+++ class/src/main/java/ro/tm/siit/classroom/w4d1/App.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ro.tm.siit.classroom.w4d1;
+
+/**
+ * Eclipse created class by File-New..-Class
+ * 
+ * @author mco
+ *
+ */
+public class App {
+
+	/**
+	 * auto-gen main method by checking main method option in New Java Class dialog
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
