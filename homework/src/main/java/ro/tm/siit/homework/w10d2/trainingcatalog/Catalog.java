751bb30bcc89cfdf9b51ee457c7e401e556be768
mco <madalin.cosma@dialogdata.de>
2016-02-22 19:49:16 +0200

    refactored Catalog T1 to store grades outside of trainee

--- homework/src/main/java/ro/tm/siit/homework/w10d2/trainingcatalog/Catalog.java
+++ homework/src/main/java/ro/tm/siit/homework/w10d2/trainingcatalog/Catalog.java
@@ -12,56 +12,65 @@ package ro.tm.siit.homework.w10d2.trainingcatalog;
  */
 public class Catalog implements TrainerCatalogInterface {
 
-	private Trainee[] trainees = new Trainee[15];
-	// private int traineesCount;
+	private TraineeGrades[] trainees = new TraineeGrades[15];
+	private int traineesCount;
 	private String name;
 
 	/**
-	 * @param name the name of the training
-	 * @param trainees the trainees
+	 * @param name
+	 *            the name of the training
+	 * @param trainees
+	 *            the trainees
 	 */
 	public Catalog(String name, Trainee... trainees) {
 		super();
 		this.name = name;
-		// for (Trainee t : trainees) {
-		// this.trainees[traineesCount++] = t;
-		// }
-		this.trainees = trainees;
-		// traineesCount = trainees.length;
+		for (Trainee t : trainees) {
+			this.trainees[traineesCount++] = new TraineeGrades(t);
+		}
 	}
 
-	// public void addTrainee(Trainee t) {
-	// this.trainees[traineesCount++] = t;
-	// }
+	public void addTrainee(Trainee t) {
+		this.trainees[traineesCount++] = new TraineeGrades(t);
+	}
 
-	/* (non-Javadoc)
-	 * @see ro.tm.siit.homework.w10d2.trainingcatalog.TrainerCatalogInterface#addGrade(java.lang.String, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ro.tm.siit.homework.w10d2.trainingcatalog.TrainerCatalogInterface#
+	 * addGrade(java.lang.String, int)
 	 */
 	public void addGrade(String name, int grade) {
-		Trainee participant = find(name);
+		TraineeGrades participant = find(name);
 		participant.addGrade(grade);
 
 	}
 
-	
-	/* (non-Javadoc)
-	 * @see ro.tm.siit.homework.w10d2.trainingcatalog.SiteManagerCatalogInterface#printGrades(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ro.tm.siit.homework.w10d2.trainingcatalog.SiteManagerCatalogInterface#
+	 * printGrades(java.lang.String)
 	 */
 	public void printGrades(String name) {
-		Trainee participant = find(name);
+		TraineeGrades participant = find(name);
 		System.out.println(participant);
 
 	}
 
-	
-	/* (non-Javadoc)
-	 * @see ro.tm.siit.homework.w10d2.trainingcatalog.SiteManagerCatalogInterface#printCatalog()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ro.tm.siit.homework.w10d2.trainingcatalog.SiteManagerCatalogInterface#
+	 * printCatalog()
 	 */
 	public void printCatalog() {
 		System.out.println("Catalog " + name + " has " + trainees.length + " trainees");
-		for (Trainee t : trainees) {
+		for (TraineeGrades t : trainees) {
 			if (t != null) {
-				System.out.println(t.getName() + " " + t.getAvgGrade());
+				System.out.println(t.trainee.getName() + " " + t.getAvgGrade());
 			}
 		}
 
@@ -79,16 +88,63 @@ public class Catalog implements TrainerCatalogInterface {
 
 	/**
 	 * searches for a Trainee with specified name
-	 * @param name the name fo the trainee
+	 * 
+	 * @param name
+	 *            the name fo the trainee
 	 * @return a Trainee object or null if not found
 	 */
-	private Trainee find(String name) {
-		for (Trainee t : trainees) {
-			if (t.getName().equals(name)) {
+	private TraineeGrades find(String name) {
+		for (TraineeGrades t : trainees) {
+			if (t.trainee.getName().equals(name)) {
 				return t;
 			}
 		}
 		return null;
+	}
+
+	private class TraineeGrades {
+		private Trainee trainee;
+		private int[] grades;
+		private int gradeCount = 0;
+
+		public TraineeGrades(Trainee trainee) {
+			super();
+			this.trainee = trainee;
+		}
+
+		/**
+		 * @param grade
+		 *            adds a new grade to trainee
+		 */
+		public void addGrade(int grade) {
+			this.grades[gradeCount++] = grade;
+
+		}
+
+		/**
+		 * @return a float as average of the grades
+		 */
+		public float getAvgGrade() {
+			int sum = 0;
+			for (int i = 0; i < gradeCount; i++) {
+				sum += grades[i];
+			}
+			return ((float) sum) / gradeCount;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			String out = trainee.getName() + " : ";
+			for (int i = 0; i < gradeCount; i++) {
+				out += grades[i] + " ";
+			}
+			return out;
+		}
 	}
 
 }
--- homework/src/main/java/ro/tm/siit/homework/w10d2/trainingcatalog/Trainee.java
+++ homework/src/main/java/ro/tm/siit/homework/w10d2/trainingcatalog/Trainee.java
@@ -8,9 +8,6 @@ package ro.tm.siit.homework.w10d2.trainingcatalog;
  *
  */
 public class Trainee {
-
-	private int[] grades = new int[10];
-	private int gradeCount;
 	private String name;
 
 	/**
@@ -23,41 +20,9 @@ public class Trainee {
 	}
 
 	/**
-	 * @param grade adds a new grade to trainee
-	 */
-	public void addGrade(int grade) {
-		this.grades[gradeCount++] = grade;
-		
-	}
-
-	/**
 	 * @return the name of the trainee
 	 */
 	public String getName() {
 		return name;
 	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		String out = name + " : ";
-		for (int i = 0; i < gradeCount; i++) {
-			out += grades[i] + " ";
-		}
-		return out;
-	}
-
-	/**
-	 * @return a float as average of the grades
-	 */
-	public float getAvgGrade() {
-		int sum = 0;
-		for (int i = 0; i < gradeCount; i++) {
-			sum += grades[i];
-		}
-		return ((float)sum)/gradeCount;
-	}
-
 }
